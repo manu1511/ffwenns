e6bbe3e5c2978759c01b7914c48286505037c9f7
Manuel Koell <manu.koell@mailbox.org>
2021-02-27 19:00:39 +0100

    add post schema and model

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    date: Date,
+    title: String,
+    content: String,
+    category: {
+      type: String,
+      index: true,
+    },
+    attachments: [String],
+    source: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
