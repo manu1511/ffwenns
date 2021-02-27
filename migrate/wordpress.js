8418e44cea40c94dfb64b2401bc8eed8799e364a
Manuel Koell <manu.koell@mailbox.org>
2021-02-27 19:02:10 +0100

    refactor with php unserialize

--- migrate/wordpress.js
+++ migrate/wordpress.js
@@ -1,67 +1,42 @@
-const url = require("url");
-const path = require("path");
 const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
+const { unserialize } = require("php-serialize");
 
-dotenv.config();
+const con = mysql.createConnection(process.env.MYSQL_URI);
 
-const query = `
+const getAttachments = async (post) => {
+  const query = "SELECT guid FROM wp_posts WHERE id IN (?)";
+  const [rows] = await con
+    .promise()
+    .query(query, [unserialize(post.attachments)]);
+
+  return rows.map((row) => new URL(row.guid).pathname);
+};
+
+const getPosts = async (limit) => {
+  const query = `
     SELECT id, post_date, post_title, post_name, post_type,
     MAX(CASE WHEN meta_key='text' THEN meta_value END) AS post_text,
     MAX(CASE WHEN meta_key='bilder' THEN meta_value END) AS attachments
     FROM wp_posts JOIN wp_postmeta ON post_id=id
     WHERE post_type IN ('einsaetze', 'uebungen', 'taetigkeiten') 
-    AND post_status='publish'
-    GROUP BY id
-    LIMIT 5`;
+    AND post_status='publish' GROUP BY id ORDER BY post_date
+    ${limit ? `LIMIT ${limit}` : ""}`;
 
-const con = mysql.createConnection(process.env.MYSQL_URI);
-
-const attachments = async (post) => {
-  const pattern = /".*?"/g;
-  const ids = post.attachments
-    .match(pattern)
-    .map((id) => parseInt(id.slice(1, -1)));
-
-  const attachments = await con
-    .promise()
-    .query("SELECT guid FROM wp_posts WHERE id IN (?)", [ids]);
-
-  const [rows] = attachments;
-  const urls = [];
-
-  rows.map((row) => {
-    const url = new URL(row.guid);
-    urls.push(url.pathname);
-  });
-
-  return urls;
-};
-
-const posts = async () => {
   const [rows] = await con.promise().query(query);
-  const posts = await Promise.all(
-    rows.map(async (row) => {
-      const rows = await attachments(row);
 
-      return {
-        date: row.post_date,
-        title: row.post_title,
-        content: row.post_text,
-        categories: row.post_type,
-        attachments: row.attachments && rows,
-        source: "wordpress",
-      };
-    })
-  );
-
-  return posts;
+  return await Promise.all(
+    rows.map(async (row) => ({
+      date: row.post_date,
+      title: row.post_title,
+      content: row.post_text,
+      category: row.post_type,
+      attachments: row.attachments && (await getAttachments(row)),
+      source: "wordpress",
+    }))
+  ).then(con.end());
 };
 
-posts().then((posts) => {
-  posts.map((post) => {
-    console.log(posts);
-  });
-
-  con.end();
-});
+module.exports = {
+  posts: getPosts,
+};
